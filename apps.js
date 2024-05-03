137eda336989cedb521b9655e5a2c22045b867c7
zukoALli <159185283+zukoALli@users.noreply.github.com>
2024-05-04 02:43:15 +0500

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,20 @@
+var personName = "\n\t IMRAN KHAN \t\n";
+console.log(personName);
+var stripped = personName.trim();
+console.log(stripped);
+console.log(5 + 3);
+console.log(11 - 3);
+console.log(4 * 2);
+console.log(16 / 2);
+var favouriteNumber = 4;
+console.log("My favourite number is ".concat(favouriteNumber, "."));
+//My name is Umar Ali
+//Date is Freruary:25:2024
+//Exercise no is 10
+//This program is going to print Message.
+console.log("Hello world this is Umar Ali");
+//My name is Umar Ali
+//Date is Freruary:25:2024
+//Exercise no is 10
+//This program is doing division.
+console.log(21 / 3);
---
+++ apps.ts
@@ -0,0 +1,25 @@
+let personName: string =`\n\t IMRAN KHAN \t\n`;
+console.log(personName);
+let stripped: string = personName.trim();
+console.log(stripped);
+
+console.log(5+3);
+console.log(11-3);
+console.log(4*2);
+console.log(16/2);
+
+let favouriteNumber: number = 4;
+console.log(`My favourite number is ${favouriteNumber}.`);
+
+//My name is Umar Ali
+//Date is Freruary:25:2024
+//Exercise no is 10
+//This program is going to print Message.
+console.log(`Hello world this is Umar Ali`);
+
+//My name is Umar Ali
+//Date is Freruary:25:2024
+//Exercise no is 10
+//This program is doing division.
+console.log(21/3);
+
